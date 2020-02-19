fd4c8ead7694e9b6d164d57c1070426ff3a99a74
Pragyan Ghimire <pragyang@gmail.com>
2020-02-19 13:24:59 -0500

    This is my second commit

--- src/com/vastika/payroll/basic/HelloServlet.java
+++ src/com/vastika/payroll/basic/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>");
